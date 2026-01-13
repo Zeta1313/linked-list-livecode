4cf93b04a34e735c9875a7ccec5b96d97b2d9ed6
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-01-13 14:48:13 -0800

    1/13 class

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,9 @@
 public class Node {
-    
+    public char value;
+    public Node next;
+
+    public Node(char x, Node y) {
+        this.value = x;
+        this.next = y;
+    }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,21 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node head = new Node('g', null);
+        Node ryan = new Node('s', new Node('v', null));
+        head.next = ryan;
+
+        System.out.println(head.next.next.value);
+        head = head.next;
+        printlist(head);
+
+    }
+
+    public static void printlist(Node start) {
+        Node current = start; 
+        while (current.next!=null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+
     }
 }
